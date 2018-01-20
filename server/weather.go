7e0b9cbaa97a24ad09c79f959b248522452c19d9
Austin Riba <austin@m51.io>
2018-01-19 16:41:02 -0800

    Allow querying for non aggregated responses:

--- server/weather.go
+++ server/weather.go
@@ -39,23 +39,49 @@ type Aggregations struct {
 	Aggregation Aggregation `json:"date(timestampmeasured30m)"`
 }
 
-type EsResponse struct {
+type EsAggResponse struct {
 	Aggregations Aggregations `json:"aggregations"`
 }
 
-type Shit struct {
-	Took float32 `json:"took"`
+type Source struct {
+	TimeStampMeasured 	string 	`json:"timestampmeasured"`
+	ValueFloat 			float32 `json:"value_float"`
+	ValueString 		string 	`json:"value_string"`
 }
 
+type SubHit struct {
+	Source Source `json:"_source"`
+}
+
+type Hit struct {
+	SubHits []SubHit `json:"hits"`
+}
+
+type EsStdResponse struct {
+	Hit Hit `json:"hits"`
+}
+
+type EsResponse interface {
+	toDatums() []Datum
+}
 type Datum struct {
 	TimeStamp string
 	Value     float32
+	ValueString string
 }
 
-func (esResponse *EsResponse) toDatums() []Datum {
+func (esStdResponse *EsStdResponse) toDatums() []Datum {
 	var datums []Datum
-	for _, bucket := range esResponse.Aggregations.Aggregation.Buckets {
-		datums = append(datums, Datum{bucket.KeyAsString, bucket.Value.Val})
+	for _, subhit := range esStdResponse.Hit.SubHits{
+		datums = append(datums, Datum{subhit.Source.TimeStampMeasured, subhit.Source.ValueFloat, subhit.Source.ValueString})
+	}
+	return datums
+}
+
+func (esAggResponse *EsAggResponse) toDatums() []Datum {
+	var datums []Datum
+	for _, bucket := range esAggResponse.Aggregations.Aggregation.Buckets {
+		datums = append(datums, Datum{bucket.KeyAsString, bucket.Value.Val, ""})
 	}
 	return datums
 }
@@ -64,10 +90,18 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Please see the documentation for the weather API")
 }
 
-func ParamsToEsQuery(site string, datumName string, start string, end string) ([]byte, error) {
+func ParamsToEsQuery(site string, datumName string, start string, end string, agg bool) ([]byte, error) {
 	buf := new(bytes.Buffer)
 	query := DatumQuery{site, datumName, start, end}
-	tmpl, err := template.ParseFiles("searchtemplate.json")
+
+	template_str := ""
+	if agg{
+		template_str = "aggsearchtemplate.json"
+	} else {
+		template_str = "searchtemplate.json"
+	}
+	tmpl, err := template.ParseFiles(template_str)
+
 	if err != nil {
 		return nil, err
 	}
@@ -76,18 +110,23 @@ func ParamsToEsQuery(site string, datumName string, start string, end string) ([
 	return buf.Bytes(), nil
 }
 
-func EsSearch(SearchString []byte) (*EsResponse, error) {
+func EsSearch(SearchString []byte, agg bool) (EsResponse, error) {
 	esUrl := "http://elasticsearch.lco.gtn:9200/_search?pretty"
 	res, err := http.Post(esUrl, "application/json", bytes.NewBuffer(SearchString))
 	if err != nil {
 		panic(err)
 	}
 	body, err := ioutil.ReadAll(res.Body)
-	var esr = new(EsResponse)
+	var esr EsResponse
 	// Golang doesn't like commas in keys
 	newbody := string(body)
 	replaced := strings.Replace(newbody, ",30m", "30m", -1)
-	err = json.Unmarshal([]byte(replaced), &esr)
+	if(agg){
+		esr = new(EsAggResponse)
+	} else {
+		esr = new(EsStdResponse)
+	}
+	err = json.Unmarshal([]byte(replaced), esr)
 	if err != nil {
 		return esr, err
 	}
@@ -107,17 +146,25 @@ func Query(w http.ResponseWriter, r *http.Request) {
 		start = start_time.Format("2006-01-02T15:04:05Z")
 	}
 	log.Println(start)
+
 	end := queryValues.Get("end")
 	if end == "" {
 		end = time.Now().UTC().Format("2006-01-02T15:04:05Z")
 	}
 	log.Println(end)
-	parsed, err := ParamsToEsQuery(site, datumName, start, end)
+
+	agg := queryValues.Get("agg")
+	aggBool := false
+	if agg == "" || agg == "true" {
+		aggBool = true
+	}
+
+	parsed, err := ParamsToEsQuery(site, datumName, start, end, aggBool)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
-	esr, err := EsSearch(parsed)
+	esr, err := EsSearch(parsed, aggBool)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
