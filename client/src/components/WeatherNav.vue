08a0a8123ef4f5786bb81e3f14cb491213796a80
Austin Riba <ariba@lco.global>
2018-01-23 14:01:35 -0800

    remove navbar and add logo to fixed menu

--- client/src/components/WeatherNav.vue
+++ client/src/components/WeatherNav.vue
@@ -3,6 +3,14 @@
   <ul class="menu-list">
     <li>
       <div class="navheader">
+        <div class="columns">
+          <div class="column">
+            <img src="https://cdn.lco.global/mainstyle/img/LCO-logo-web.jpg" alt="Las Cumbres Observatory Weater" class="image logo">
+          </div>
+          <div class="column align-bottom">
+            <h2 class="is-size-3-desktop is-size-4-mobile">Weather</h2>
+          </div>
+        </div>
         <div class="columns level mini-info">
           <div class="column place is-two-thirds heading">Location</div>
           <div class="column time is-one-fifth heading">UTC</div>
@@ -39,5 +47,13 @@
 }
 .navheader {
   padding: 0.5em 0.75em;
+}
+.align-bottom {
+  display: flex;
+  align-items: center;
+}
+
+.logo {
+  max-width: 9rem;
 }
 </style>
