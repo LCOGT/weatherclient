bbd069b5a6910ce579f639564e5b72cbca32e2fc
Raleigh Littles <rlittles@lco.global>
2018-05-22 15:22:36 -0700

    Removed unused store methods

--- client/src/store.js
+++ client/src/store.js
@@ -17,27 +17,6 @@ export default new Vuex.Store({
     },
     end: state => {
       return state.end;
-    }, // from this: https://vuex.vuejs.org/en/getters.html it says we have to return a function if we want
-    // to pass arguments to getters?
-    site_status: (state) => (site_code) => {
-      /* There should be an object in state that has something like:
-
-        {
-        .
-        .
-        .
-          site_code: status
-        }
-
-      */
-      //return state.site_code[status];
-
-      function getSiteStatus()
-      {
-        return state.site_code[status];
-      }
-
-      return getSiteStatus();
     }
   },
   mutations:{
@@ -49,12 +28,6 @@ export default new Vuex.Store({
     },
     setRange(state, text){
       state.range = text;
-    },
-    setSiteStatus(state, site_code, status) {
-      // Since we can't do: state.site_code.status, you have to use the Vue.set() function to set the property
-      // for an object
-
-      Vue.set(state, site_code, status);
     }
   }
 });
