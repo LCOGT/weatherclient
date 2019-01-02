1bb2949225154f2a8537a30dba2786124173facb
Austin Riba <austin@m51.io>
2019-01-02 13:49:01 -0800

    webpack upgraded

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -89,22 +89,15 @@ module.exports = {
 }
 
 if (process.env.NODE_ENV === 'production') {
+  module.exports.mode = 'production'
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
     new webpack.LoaderOptionsPlugin({
       minimize: true
     })
   ])
+}
+else {
+  module.exports.mode = 'development';
 }
